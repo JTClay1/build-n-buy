c221990ed72821606a1fa3ea20eba149672814b6
Joshua Clay <josh.t.clay1995@gmail.com>
2026-03-30 19:34:56 -0700

    chore: wire up Flask app, configure extensions, and add initial db migrations

---
+++ server/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    # Database config
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # JWT config
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'super-secret-dev-key-change-later')
